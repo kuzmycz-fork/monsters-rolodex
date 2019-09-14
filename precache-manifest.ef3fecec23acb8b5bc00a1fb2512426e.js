7e1a49d99f5d9b7a00a4bf6c498ba718db87eccd
Mark Kuzmycz <kuzmycz@Marks-MacBook-Pro.local>
2019-09-14 17:35:48 +1000

    Updates

---
+++ precache-manifest.ef3fecec23acb8b5bc00a1fb2512426e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9fb00e4b56bf26752483e3bfce1ca16",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d56cffb3add384254e88",
+    "url": "/monsters-rolodex/static/css/main.d9f82ad7.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/monsters-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "d56cffb3add384254e88",
+    "url": "/monsters-rolodex/static/js/main.71b7b8de.chunk.js"
+  },
+  {
+    "revision": "5f535753dcec63ef3476",
+    "url": "/monsters-rolodex/static/js/runtime~main.7352cc78.js"
+  }
+]);
